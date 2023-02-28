d37a342bfde12c85efa5f7333c9afbc678280738
minhnhut170701 <minhnhut170701@gmail.com>
2023-02-28 15:48:53 +0700

    fix

--- client/feature/Cart/CartService.ts
+++ client/feature/Cart/CartService.ts
@@ -1,5 +1,4 @@
 
-const URL = 'http://localhost:3003/api/cart/addItem'
 
 type CartType = {
   productName: string,
@@ -10,14 +9,14 @@ type CartType = {
 
 // get list item in cart
 const fetchItemCart = async (userId: string) =>{
-  const respone = await fetch(`http://localhost:3003/api/cart/${userId}`)
+  const respone = await fetch(`https://nextjs13-ecommerce.onrender.com/api/cart/${userId}`)
   const jsonen = await respone.json()
   return jsonen
 }
 
 // add to cart
 const fetchAdd = async (cartData: CartType, userId: string) =>{
-  const respone = await fetch(`http://localhost:3003/api/cart/addItem/${userId}`, {
+  const respone = await fetch(`https://nextjs13-ecommerce.onrender.com/api/cart/addItem/${userId}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -30,7 +29,7 @@ const fetchAdd = async (cartData: CartType, userId: string) =>{
 
 // delete item cart
 const fetchRemoveItem = async (cartId: string) =>{
-  const respone = await fetch(`http://localhost:3003/api/cart/deleteItem/${cartId}`, {
+  const respone = await fetch(`https://nextjs13-ecommerce.onrender.com/api/cart/deleteItem/${cartId}`, {
     method: 'DELETE',
   })
   const jsonen = await respone.json()
@@ -40,7 +39,7 @@ const fetchRemoveItem = async (cartId: string) =>{
 
 // increament item cart
 const fetchIncrementItem = async (cartId: string) =>{
-  const respone = await fetch(`http://localhost:3003/api/cart/increment/${cartId}`, {
+  const respone = await fetch(`https://nextjs13-ecommerce.onrender.com/api/cart/increment/${cartId}`, {
     method: 'PUT',
   })
   const jsonen = await respone.json()
@@ -48,7 +47,7 @@ const fetchIncrementItem = async (cartId: string) =>{
 }
 // decrement item cart
 const fetchDecrementItem = async (cartId: string) =>{
-  const respone = await fetch(`http://localhost:3003/api/cart/decrement/${cartId}`, {
+  const respone = await fetch(`https://nextjs13-ecommerce.onrender.com/api/cart/decrement/${cartId}`, {
     method: 'PUT',
   })
   const jsonen = await respone.json()
@@ -57,7 +56,7 @@ const fetchDecrementItem = async (cartId: string) =>{
 
 // clean item cart
 const fetchCleanItem = async (userId: string) =>{
-  const respone = await fetch(`http://localhost:3003/api/cart/clearCart/${userId}`, {
+  const respone = await fetch(`https://nextjs13-ecommerce.onrender.com/api/cart/clearCart/${userId}`, {
     method: 'POST',
   })
   const jsonen = await respone.json()
