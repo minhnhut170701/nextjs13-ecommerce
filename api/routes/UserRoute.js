a84f7d6f56838a88109ffe4df748ed6295203bf5
minhnhut170701 <minhnhut170701@gmail.com>
2023-10-12 16:17:40 +0700

    change domain

--- api/routes/UserRoute.js
+++ api/routes/UserRoute.js
@@ -158,7 +158,7 @@ route.post('/forgot', async (req, res) =>{
       subject: 'Password Reset',
       text: `You are receiving this email because you (or someone else) has requested the reset of the password for your account.\n\n
       Please click on the following link, or paste this into your browser to complete the process:\n\n
-      https://nextjs13-ecommerce-five.vercel.app/authenticate/accpet?token=${token}\n\n
+      https://food-delivery-beta-three.vercel.app/authenticate/accpet?token=${token}\n\n
       If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 
